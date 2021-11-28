2bd5986bf86df5e2daf267f52ebe1cbb8bb507b9
mitriifd <idimka9@gmail.com>
2021-11-29 00:08:00 +0500

    атрибут key

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,11 +4,12 @@ import './employees-list.css';
 
 const EmployeesList = ({data}) => {
 
-    const elements = data.map(item => {
+    const elements = data.map((item, key) => {
         // return <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} />
 
+        const {id, ...itemProps} = item;
         // Развертывание пропсов через оператор расширения
-        return <EmployeesListItem {...item} />
+        return <EmployeesListItem key={id} {...itemProps} />
     })
 
     return (
