0f936b39b1f0f651f6a77597adb2b346b259e378
mitriifd <idimka9@gmail.com>
2021-11-29 16:12:36 +0500

    иммутабельность состояния и собственные события

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,14 +2,18 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({ data, onDelete }) => {
 
-    const elements = data.map((item, key) => {
+    const elements = data.map(item => {
         // return <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} />
 
-        const {id, ...itemProps} = item;
+        const { id, ...itemProps } = item;
         // Развертывание пропсов через оператор расширения
-        return <EmployeesListItem key={id} {...itemProps} />
+        return <EmployeesListItem
+            key={id}
+            {...itemProps}
+            onDelete={() => onDelete(id)}
+        />
     })
 
     return (
